78e5e75716fed26eaf62fb379a6e498ae3477120
Paola Medina <pmmedinab@unal.edu.co>
2019-01-23 19:59:38 -0500

    Edit simulator One row

--- simulatorOneRows.rb
+++ simulatorOneRows.rb
@@ -25,47 +25,40 @@ class Simulator
   end
   def caja()
     #al ser una sola fila solo ahi una sola persona por caja
-    if $caja1.length <= 0
-      $caja1.push element
+    $users.each_with_index do |element, index|
+      #Cada 1 a 3 minutos llega un cliente a una fila
+      $time=$time + rand(1..3)
+      if $caja1.length <= 0
+        $caja1.push element
+      elsif $caja2.length <= 0
+        $caja2.push element
+      elsif $caja3.length <= 0
+        $caja3.push element
+      elsif $caja4.length <= 0
+        $caja4.push element
+      else
+        attend()
+      end
+
     end
-    if $caja2.length <= 0
-      $caja1.push element
-    end
-    if $caja3.length <= 0
-      $caja1.push element
-    end
-    if $caja4.length <= 0
-      $caja1.push element
-    end
-    attend();
+
   end
   def attend()
-    i=1
-    while i <= $users.length
+    if $caja1.length >= 1
       $time=$time + rand(1..4)
-      cajaAtendida = rand(1..4)
-      if cajaAtendida == 1
-        if $caja1.length > 0
-          $caja1.delete_at(0)
-          i=i+1
-        end
-      elsif cajaAtendida == 2
-        if $caja2.length > 0
-          $caja2.delete_at(0)
-          i=i+1
-        end
-      elsif cajaAtendida == 3
-        if $caja3.length > 0
-          $caja3.delete_at(0)
-          i=i+1
-        end
-      elsif cajaAtendida == 4
-        if $caja4.length > 0
-          $caja4.delete_at(0)
-          i=i+1
-        end
-      end
-      caja();
+        $caja1.delete_at(0)
+    end
+    if $caja2.length >= 1
+      $time=$time + rand(1..4)
+        $caja2.delete_at(0)
+    end
+    if $caja3.length >= 1
+      $time=$time + rand(1..4)
+        $caja3.delete_at(0)
+    end
+    if $caja4.length >= 1
+      $time=$time + rand(1..4)
+        $caja4.delete_at(0)
     end
   end
 end
